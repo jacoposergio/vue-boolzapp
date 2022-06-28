4c6ab330fcfa583efdee35d598f75020ccc34eb1
jacoposergio <djago32@gmail.com>
2022-06-28 15:31:45 +0200

    css making

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@
 // Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
 // Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
 
-alert('bellapevvoi')
-
 
 Vue.config.devtools = true
 
